c80005ac612b5fba74833e5a227202ef864b4c4a
unknown <dussarajesh123@gmail.com>
2026-02-08 22:16:01 +0530

    my first page

---
+++ chan.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
